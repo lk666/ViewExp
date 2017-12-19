58bfaed10f6a683b922e1f76e1247d06101ab6f6
LK <447451103@qq.com>
2017-12-20 00:00:48 +0800

    ripple例子

--- app/src/main/java/lk/cn/com/newbanner/MainActivity.java
+++ app/src/main/java/lk/cn/com/newbanner/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     {
         list.add(new ActivityItem("banner切换动画及新的自定义banner指示器", BannerActivity.class));
         list.add(new ActivityItem("metal design简单实例", AppBarActivity.class));
+        list.add(new ActivityItem("Ripple涟漪效果", RippleActivity.class));
     }
 
     @Override
---
+++ app/src/main/java/lk/cn/com/newbanner/RippleActivity.java
@@ -0,0 +1,15 @@
+package lk.cn.com.newbanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Ripple使用
+ */
+public class RippleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ripple);
+    }
+}
